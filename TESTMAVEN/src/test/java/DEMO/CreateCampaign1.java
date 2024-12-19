37e9d1f2e8afc8c516bedece7e4b1fa7a64b9283
josep <josep@LAPTOP-0VDO5D5M>
2024-12-19 11:48:02 +0530

    commit1Branch

--- TESTMAVEN/src/test/java/DEMO/CreateCampaign1.java
+++ TESTMAVEN/src/test/java/DEMO/CreateCampaign1.java
@@ -14,6 +14,7 @@ import ObjectRepo.VtigerHomePage;
 
 public class CreateCampaign1 extends BaseClass 
 {	
+	//enginner1------->branch1
 	@Test
 	public void createCampaignTest() throws Throwable {
 		
